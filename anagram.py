0dcfbd5fedca32b972245db1d81c76e5e174a6b4
akhilpulimala <64695974+akhilpulimala@users.noreply.github.com>
2023-08-05 19:55:13 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,1 +1,22 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+    map_1 = {}
+    map_2 = {}
 
+    if len(s) != len(t):
+        return False 
+
+    for ele in s:
+        map_1[ele] = map_1.get(ele,0) + 1
+    
+    for ele in t:
+        map_1[ele] = map_1.get(ele,0) + 1
+
+    if map_1 == map_2:
+        return True
+    else:
