1ce8cee7aa16ef046057401505342bb02a783483
akhilpulimala <64695974+akhilpulimala@users.noreply.github.com>
2023-08-05 13:24:07 +0530

    Update duplicate_array.py
    duplicate in array

--- duplicate_array.py
+++ duplicate_array.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        checker = set()
+        for ele in nums:
+            if ele in checker:
+                return True
+            else:
+                checker.add(ele)
+        return False
 
