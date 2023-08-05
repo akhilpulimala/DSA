3c9a5d4feedc5983c46b9b60765e5ef4747a4542
akhilpulimala <64695974+akhilpulimala@users.noreply.github.com>
2023-08-05 19:57:03 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -5,18 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-    map_1 = {}
-    map_2 = {}
+        map_1 = {}
+        map_2 = {}
 
-    if len(s) != len(t):
-        return False 
+        if len(s) != len(t):
+            return False 
 
-    for ele in s:
-        map_1[ele] = map_1.get(ele,0) + 1
-    
-    for ele in t:
-        map_1[ele] = map_1.get(ele,0) + 1
+        for ele in s:
+            map_1[ele] = map_1.get(ele,0) + 1
+        
+        for ele in t:
+            map_2[ele] = map_2.get(ele,0) + 1
 
-    if map_1 == map_2:
-        return True
-    else:
+        if map_1 == map_2:
+            return True
+        else:
+            return False
+
+     
